d23e4081baac88b13870713a03074084eb085b2d
DaveCalaway <davide.gariselli@gmail.com>
2016-10-21 15:33:49 +0200

    V.02
    Added Colours and plot

--- ImageMatrix.m
+++ ImageMatrix.m
@@ -1,11 +1,12 @@
 %% Pixel Region analyzer for matlab
-% Git: DaveCalaway - V0.1
+% Git: DaveCalaway - V0.2
 % The script analyze the central part of picture, and fills the x-by-3 matrix
 % with RBG colors. Does not matter if the pic is BW.
 clc
 clear all
 close all
-image_name = '420nm.jpeg';
+image_name = 'manettino_600.jpg';
+vettore=[400:5:800];
 tic
 pos=1;
 %% Show image and read the dimension
@@ -27,6 +28,19 @@ for x=(Width*3):(Width*4)
         pos=pos+1;
     end
 end
+%% Allocate imported array to column variable names
+figure()
+ grid on
+ hold on
+% Apmlitude normalized
+ nAmpl = rgbColor/max(abs(rgbColor(:)));
+ % Plots each matrix column as a separate line
+ p = plot(nAmpl());
+ p(1).Color = 'r';
+ p(2).Color = 'g';
+ p(3).Color = 'b';
+%Plot limits
+%xlim([350 850])
 %Find the mean value of each column.
 RGB = mean(rgbColor);
 disp('Done!')
